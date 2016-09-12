d6f05473216a8e55c30847c05ef926693d6892a7
BernardBot <bernardo.reinier@gmail.com>
2016-09-12 09:35:59 +0000

    added javadoc comments

--- src/nl/tudelft/asteroids/game/AsteroidsGame.java
+++ src/nl/tudelft/asteroids/game/AsteroidsGame.java
@@ -7,12 +7,26 @@ import org.newdawn.slick.state.StateBasedGame;
 
 import nl.tudelft.asteroids.game.states.PlayState;
 
+/**
+ * Asteroids game with multiple states.
+ * @author Bernard
+ *
+ */
 public class AsteroidsGame extends StateBasedGame {
 	
+	/**
+	 * Constructor; specifying the title of the game.
+	 * 
+	 * @param	title 
+	 * 			Title of the game, in this case "Asteroids"
+	 */
 	public AsteroidsGame(String title) {
 		super(title);
 	}
-
+	
+	/**
+	 * Initializes the states of the game, and sets the background sprite.
+	 */
 	@Override
 	public void initStatesList(GameContainer arg0) throws SlickException {
 		Image background = new Image("resources/BG4.jpg");
--- src/nl/tudelft/asteroids/game/states/MenuState.java
+++ src/nl/tudelft/asteroids/game/states/MenuState.java
@@ -7,33 +7,54 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+/**
+ * The menu state of the Asteroids game.
+ * 
+ * @author Bernard
+ *
+ */
 public class MenuState extends BasicGameState {
-	
+
 	private final Image background;
-	
+
+	/**
+	 * Constructor; sets background sprite.
+	 * 
+	 * @param background
+	 */
 	public MenuState(Image background) {
 		this.background = background;
 	}
 
+	/**
+	 * Empty override method.
+	 */
 	@Override
 	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
-		
-		
+
 	}
 
+	/**
+	 * Empty override method.
+	 */
 	@Override
 	public void render(GameContainer arg0, StateBasedGame arg1, Graphics g) throws SlickException {
 		g.drawImage(background, 0, 0);
-		
-		
+
 	}
 
+	/**
+	 * Empty override method.
+	 */
 	@Override
 	public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	/**
+	 * Empty override method.
+	 */
 	@Override
 	public int getID() {
 		// TODO Auto-generated method stub

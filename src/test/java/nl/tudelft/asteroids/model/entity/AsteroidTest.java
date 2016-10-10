5b79947b510ce8c875ca00acaf08d1994b298a6f
Jasper Ketelaar <jketelaar@student.tudelft.nl>
2016-10-11 01:54:27 +0200

    Fixed logger tests

--- src/test/java/nl/tudelft/asteroids/model/entity/AsteroidTest.java
+++ src/test/java/nl/tudelft/asteroids/model/entity/AsteroidTest.java
@@ -1,10 +1,17 @@
 package nl.tudelft.asteroids.model.entity;
 
 import org.junit.Test;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
 
-public class AsteroidTest {
+import nl.tudelft.asteroids.TestWithDisplay;
+import nl.tudelft.asteroids.model.entity.dyn.explodable.Asteroid;
+
+public class AsteroidTest extends TestWithDisplay {
+	
 	@Test
-	public void testAsteroidConstructor() {
+	public void testAsteroidConstructor() throws SlickException {
+		Asteroid asteroid = new Asteroid(new Vector2f(0, 0), 0, 1);
 
 	}
 }

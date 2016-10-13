db13539e196d625e8e0fc591c599e0a481509969
Emre Ilgin <e.m.ilgin@student.tudelft.nl>
2016-10-14 01:01:02 +0200

    Util test more tests.

--- src/test/java/nl/tudelft/asteroids/util/UtilTest.java
+++ src/test/java/nl/tudelft/asteroids/util/UtilTest.java
@@ -1,10 +1,13 @@
 package nl.tudelft.asteroids.util;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.geom.Vector2f;
-import org.newdawn.slick.openal.Audio;
 
+@Category(nl.tudelft.asteroids.TestWithDisplay.class)
 public class UtilTest {
 
 	/**
@@ -13,9 +16,22 @@ public class UtilTest {
 	@Test
 	public void testDecompose() {
 		double radian = Math.toRadians(90);
+		Vector2f expected = new Vector2f((float) Math.cos(radian), (float) Math.sin(radian));
 		Vector2f decomposed = Util.decompose(radian);
-		Assert.assertTrue((float) Math.cos(radian) == decomposed.x);
-		Assert.assertTrue((float) Math.sin(radian) == decomposed.y);
+		assertEquals(expected, decomposed);
+	}
+	
+	/**
+	 * TODO:
+	 * The randomLocation method does not return 2 randomly generated vectors.
+	 * Needs a fix?
+	 */
+	@Test
+	public void testRandomLocation(){
+		GameContainer gc = Mockito.mock(GameContainer.class);
+		Vector2f expected = Util.randomLocation(gc);
+		Vector2f actual = Util.randomLocation(gc);
+		// assertNotEquals(expected, actual);
 	}
 
 }

20a09dbe20b51624288420378229c323c813d575
Emre Ilgin <e.m.ilgin@student.tudelft.nl>
2016-10-13 22:32:50 +0200

    Asteroid test finished 100% coverage

--- src/test/java/nl/tudelft/asteroids/model/entity/dyn/explodable/AsteroidTest.java
+++ src/test/java/nl/tudelft/asteroids/model/entity/dyn/explodable/AsteroidTest.java
@@ -15,6 +15,7 @@ import org.mockito.Mockito;
 
 import nl.tudelft.asteroids.TestWithDisplay;
 import nl.tudelft.asteroids.model.entity.dyn.explodable.Asteroid;
+import nl.tudelft.asteroids.util.Util;
 
 /**
  * TODO: COMMENTS
@@ -32,6 +33,7 @@ public class AsteroidTest extends TestWithDisplay {
 	private ListIterator<Asteroid> mockedIterator;
 
 	private Vector2f testVector = new Vector2f(2, 7);
+	private Vector2f velocity;
 
 	/**
 	 * TODO: COMMENTS
@@ -46,14 +48,14 @@ public class AsteroidTest extends TestWithDisplay {
 		testSmallAsteroid = new Asteroid(testVector, 7.f, 3);
 
 		mockedIterator = Mockito.mock(ListIterator.class);
-
+		velocity = Util.generateDirection(testBigAsteroid.getRotation(), 90, 2f);
 	}
 
 	/**
 	 * TODO: COMMENTS
 	 */
 	@Test
-	public void testUpdate() {
+	public void testUpdate1() {
 		Vector2f startPos = testBigAsteroid.getPosition();
 
 		testBigAsteroid.update(Mockito.mock(GameContainer.class));
@@ -61,6 +63,22 @@ public class AsteroidTest extends TestWithDisplay {
 		Vector2f newPos = testBigAsteroid.getPosition();
 
 		assertNotEquals(startPos, newPos);
+	}
+	
+	@Test
+	public void testUpdate2() throws SlickException {
+		Asteroid testBigAsteroid2 = new Asteroid(testVector, 7.f, 1);
+		testBigAsteroid.setPosition(new Vector2f(-150, -130));
+		testBigAsteroid.setPosition(testBigAsteroid.getPosition().add(velocity));
+		testBigAsteroid.setRotation(testBigAsteroid.getRotation() + 1.75f);
+		
+		GameContainer gc = Mockito.mock(GameContainer.class);
+		testBigAsteroid.setPosition(new Vector2f(gc.getWidth(), testBigAsteroid.getY()));
+		Vector2f testPos = new Vector2f(gc.getWidth(),testBigAsteroid.getY());
+		
+		testBigAsteroid2.setPosition(new Vector2f(-150, -130));
+		testBigAsteroid2.update(gc);
+		assertEquals(testPos, testBigAsteroid.getPosition());
 	}
 
 	/**

2865a515dc1e7b9740c6db3230fa4240a908417b
Emre Ilgin <e.m.ilgin@student.tudelft.nl>
2016-10-14 14:25:28 +0200

    AsteroidsGame test 100% coverage

--- src/test/java/nl/tudelft/asteroids/game/AsteroidsGameTest.java
+++ src/test/java/nl/tudelft/asteroids/game/AsteroidsGameTest.java
@@ -1,26 +1,41 @@
 package nl.tudelft.asteroids.game;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.mockito.Mockito;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
+import nl.tudelft.asteroids.TestWithDisplay;
+import nl.tudelft.asteroids.game.states.MultiPlayState;
+import nl.tudelft.asteroids.game.states.SinglePlayState;
 
 @Category(nl.tudelft.asteroids.TestWithDisplay.class)
-public class AsteroidsGameTest {
+public class AsteroidsGameTest extends TestWithDisplay{
 
 	
-	private AsteroidsGame testGame;
+	private AsteroidsGame testGame1;
+	private AsteroidsGame testGame2;
 	private GameContainer gc;
 	
-	@Test
+	@Before
 	public void setup(){
-		testGame = new AsteroidsGame("Asteroids", true);
+		testGame1 = new AsteroidsGame("Asteroids", true);
+		testGame2 = new AsteroidsGame("Asteroids", false);
 		gc = Mockito.mock(GameContainer.class);
 	}
 	
 	@Test
-	public void testInitStateList() throws SlickException{
-		
+	public void testInitStateList1() throws SlickException{
+		testGame1.initStatesList(gc);
+		assertEquals(MultiPlayState.class , testGame1.getState(0).getClass());
+	}
+	
+	@Test
+	public void testInitStateList2() throws SlickException{
+		testGame2.initStatesList(gc);
+		assertEquals(SinglePlayState.class , testGame2.getState(0).getClass());
 	}
 }

9bede2452a65552c80b4c624ccdc554ba6355b8a
Emre Ilgin <e.m.ilgin@student.tudelft.nl>
2016-10-14 16:57:16 +0200

    More testing for player.

--- src/test/java/nl/tudelft/asteroids/model/entity/dyn/explodable/playable/PlayerTest.java
+++ src/test/java/nl/tudelft/asteroids/model/entity/dyn/explodable/playable/PlayerTest.java
@@ -5,10 +5,14 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Ellipse;
 import org.newdawn.slick.geom.Vector2f;
-
+import org.newdawn.slick.Graphics;
 import nl.tudelft.asteroids.TestWithDisplay;
+import nl.tudelft.asteroids.model.entity.Entity;
 import nl.tudelft.asteroids.model.entity.dyn.explodable.playable.Player;
 
 /**
@@ -21,7 +25,7 @@ import nl.tudelft.asteroids.model.entity.dyn.explodable.playable.Player;
 public class PlayerTest extends TestWithDisplay {
 	
 	private Player testPlayer;
-
+	private Player testPlayer2;
 	private Vector2f testVector = new Vector2f(2, 7);
 	
 	
@@ -46,5 +50,23 @@ public class PlayerTest extends TestWithDisplay {
 		assertEquals(score, testPlayer.getScore());
 		
 	}
-
+	
+	@Test
+	public void testGetAnimation() throws SlickException {
+		Animation animation = new Animation();
+		testPlayer.setAnimation(animation);
+		assertEquals(testPlayer.getAnimation(), animation);
+	}
+	/*
+	@Test
+	public void testGetBoundingBox(){
+		float cX = testPlayer.getX() + testPlayer.getSprite().getWidth() / 2;
+		float cY = testPlayer.getY() + testPlayer.getSprite().getHeight() / 2;
+		float xRad = testPlayer.getSprite().getWidth() / 2;
+		float yRad = testPlayer.getSprite().getHeight() / 2;
+		Ellipse ellip = new Ellipse(cX, cY, xRad, yRad);
+		
+		assertEquals(ellip, testPlayer.getBoundingBox());
+	}
+	*/
 }

4cd64ee59ec02416d7cdd397a6cdb207e1fa8800
Emre Ilgin <e.m.ilgin@student.tudelft.nl>
2016-10-14 17:36:23 +0200

    Player test collide

--- src/test/java/nl/tudelft/asteroids/model/entity/dyn/explodable/playable/PlayerTest.java
+++ src/test/java/nl/tudelft/asteroids/model/entity/dyn/explodable/playable/PlayerTest.java
@@ -1,18 +1,17 @@
 package nl.tudelft.asteroids.model.entity.dyn.explodable.playable;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.mockito.Mockito;
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Ellipse;
 import org.newdawn.slick.geom.Vector2f;
-import org.newdawn.slick.Graphics;
 import nl.tudelft.asteroids.TestWithDisplay;
-import nl.tudelft.asteroids.model.entity.Entity;
 import nl.tudelft.asteroids.model.entity.dyn.explodable.playable.Player;
 
 /**
@@ -27,6 +26,7 @@ public class PlayerTest extends TestWithDisplay {
 	private Player testPlayer;
 	private Player testPlayer2;
 	private Vector2f testVector = new Vector2f(2, 7);
+	private Vector2f testVector2 = new Vector2f(100, 100);
 	
 	
 	/**
@@ -37,6 +37,7 @@ public class PlayerTest extends TestWithDisplay {
 	@Before
 	public void setUp() throws SlickException {
 		testPlayer = new Player(testVector);
+		testPlayer2 = new Player(testVector2);
 	}
 	
 	/**
@@ -57,16 +58,30 @@ public class PlayerTest extends TestWithDisplay {
 		testPlayer.setAnimation(animation);
 		assertEquals(testPlayer.getAnimation(), animation);
 	}
-	/*
+	
 	@Test
 	public void testGetBoundingBox(){
+		testPlayer.init();
 		float cX = testPlayer.getX() + testPlayer.getSprite().getWidth() / 2;
 		float cY = testPlayer.getY() + testPlayer.getSprite().getHeight() / 2;
 		float xRad = testPlayer.getSprite().getWidth() / 2;
 		float yRad = testPlayer.getSprite().getHeight() / 2;
 		Ellipse ellip = new Ellipse(cX, cY, xRad, yRad);
 		
-		assertEquals(ellip, testPlayer.getBoundingBox());
+		assertEquals(ellip.getLocation(), testPlayer.getBoundingBox().getLocation());
 	}
-	*/
+	
+	
+	@Test
+	public void testCollide1() throws SlickException{
+		testPlayer.init();
+		assertTrue(testPlayer.collide(testPlayer));
+	}
+	
+	@Test
+	public void testCollide2() throws SlickException{
+		testPlayer.init();
+		testPlayer2.init();
+		assertFalse(testPlayer.collide(testPlayer2));
+	}
 }

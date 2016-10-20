25b00524e2b14891fcc710bbec96699d3a7e6758
Emre Ilgin <e.m.ilgin@student.tudelft.nl>
2016-10-20 23:25:43 +0200

    Start PowerUpFactory testing.

---
+++ src/test/java/nl/tudelft/asteroids/factory/PowerupFactoryTest.java
@@ -0,0 +1,21 @@
+package nl.tudelft.asteroids.factory;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import nl.tudelft.asteroids.TestWithDisplay;
+
+@Category(nl.tudelft.asteroids.TestWithDisplay.class)
+public class PowerupFactoryTest extends TestWithDisplay{
+
+	@Before
+	public void setUp(){
+		
+	}
+	
+	@Test
+	public void testPowerUp(){
+		
+	}
+	
+}

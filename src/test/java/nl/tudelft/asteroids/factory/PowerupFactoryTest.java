b85a661933a372669986d25e05df4edc6d4094e3
Emre Ilgin <e.m.ilgin@student.tudelft.nl>
2016-10-21 00:19:05 +0200

    Test PowerupFactory, start MenuTest

--- src/test/java/nl/tudelft/asteroids/factory/PowerupFactoryTest.java
+++ src/test/java/nl/tudelft/asteroids/factory/PowerupFactoryTest.java
@@ -1,21 +1,41 @@
 package nl.tudelft.asteroids.factory;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+import org.newdawn.slick.GameContainer;
+
 import nl.tudelft.asteroids.TestWithDisplay;
+import nl.tudelft.asteroids.model.entity.stat.PowerUp;
 
 @Category(nl.tudelft.asteroids.TestWithDisplay.class)
 public class PowerupFactoryTest extends TestWithDisplay{
 
+	private PowerupFactory powerupFactory;
+	private GameContainer gameContainer;
+	
 	@Before
 	public void setUp(){
-		
+		powerupFactory = new PowerupFactory();
+		gameContainer = Mockito.mock(GameContainer.class);
 	}
 	
 	@Test
 	public void testPowerUp(){
-		
+		PowerUp powerUp = powerupFactory.create(gameContainer);
+		assertEquals(powerUp.getClass(), PowerUp.class);
 	}
+	
+	@Test
+	public void testRequiresPowerup(){
+		boolean check = powerupFactory.requiresPowerup();
+		assertFalse(check);
+	}
+	
+	
 	
 }
--- src/test/java/nl/tudelft/asteroids/game/states/menu/MenuTest.java
+++ src/test/java/nl/tudelft/asteroids/game/states/menu/MenuTest.java
@@ -1,5 +1,20 @@
 package nl.tudelft.asteroids.game.states.menu;
 
-public class MenuTest {
+import org.junit.Before;
+import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
+import org.newdawn.slick.gui.GUIContext;
 
+import nl.tudelft.asteroids.TestWithDisplay;
+
+@Category(nl.tudelft.asteroids.TestWithDisplay.class)
+public class MenuTest extends TestWithDisplay{
+	
+	private Menu menu;
+	
+	@Before
+	public void setUp(){
+		menu = new Menu(Mockito.mock(GUIContext.class));
+	}
+	
 }

1d19c8ff46ff8fdfb2ae931a487d2709e9bc7b63
BernardBot <bernardo.reinier@gmail.com>
2016-10-31 19:16:07 +0000

    Added @Category

--- src/test/java/nl/tudelft/asteroids/model/entity/EntityTest.java
+++ src/test/java/nl/tudelft/asteroids/model/entity/EntityTest.java
@@ -3,12 +3,14 @@ package nl.tudelft.asteroids.model.entity;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Vector2f;
 
 import nl.tudelft.asteroids.TestWithDisplay;
 import nl.tudelft.asteroids.model.entity.dyn.explodable.Asteroid;
 
+@Category(nl.tudelft.asteroids.TestWithDisplay.class)
 public class EntityTest extends TestWithDisplay{
 	
 	private Asteroid asteroid1;

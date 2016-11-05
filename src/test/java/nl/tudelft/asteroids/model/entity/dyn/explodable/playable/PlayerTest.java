edf1ce5a0e733245848b7e9a80ee7c39e0af7aa0
Emre Ilgin <e.m.ilgin@student.tudelft.nl>
2016-11-05 17:36:38 +0100

    Last tests.

--- src/test/java/nl/tudelft/asteroids/model/entity/dyn/explodable/playable/PlayerTest.java
+++ src/test/java/nl/tudelft/asteroids/model/entity/dyn/explodable/playable/PlayerTest.java
@@ -4,15 +4,22 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.mockito.Mockito;
 import org.newdawn.slick.Animation;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Ellipse;
 import org.newdawn.slick.geom.Vector2f;
 import nl.tudelft.asteroids.TestWithDisplay;
 import nl.tudelft.asteroids.model.entity.dyn.explodable.playable.Player;
+import nl.tudelft.asteroids.model.entity.stat.NullPowerUp;
+import nl.tudelft.asteroids.model.entity.stat.PowerUp;
 
 /**
  * TODO: COMMENTS
@@ -83,5 +90,54 @@ public class PlayerTest extends TestWithDisplay {
 		testPlayer.init();
 		testPlayer2.init();
 		assertFalse(testPlayer.collide(testPlayer2));
+	}
+	
+	@Test
+	public void testCanFire(){
+		assertTrue(testPlayer.canFire());
+	}
+	
+	@Test
+	public void testDisableFire(){
+		testPlayer.disableFire();
+		assertFalse(testPlayer.canFire());
+	}
+	
+	@Test
+	public void testGetFiredBullets(){
+		assertEquals(testPlayer.getFiredBullets(), testPlayer.getFiredBullets());
+	}
+	
+	@Test
+	public void testGetPowerUp(){
+		PowerUp expected = new NullPowerUp();
+		testPlayer.setPowerUp(expected);
+		assertEquals(expected, testPlayer.getPowerUp());
+	}
+	
+	@Test
+	public void testGetUpKey(){
+		assertEquals(Input.KEY_UP, testPlayer.getUpKey());
+	}
+	
+	@Test
+	public void testGetRightKey(){
+		assertEquals(Input.KEY_RIGHT, testPlayer.getRightKey());
+	}
+	
+	@Test
+	public void testGetLeftKey(){
+		assertEquals(Input.KEY_LEFT, testPlayer.getLeftKey());
+	}
+	
+	@Test
+	public void testGetShootKey(){
+		assertEquals(Input.KEY_RCONTROL, testPlayer.getShootKey());
+	}
+	
+	@Test
+	public void testBindKeys(){
+		testPlayer.bindKeys(0, 1, 2, 3);
+		assertEquals(0, testPlayer.getUpKey());
 	}
 }
